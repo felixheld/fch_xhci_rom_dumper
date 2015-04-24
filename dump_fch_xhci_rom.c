3e39c5effb60c299957abef8ddcc2c1a55a1705b
Felix Held <felix-github@felixheld.de>
2015-04-24 14:23:06 +0200

    some code cleanups
    error handling is still missing...

--- dump_fch_xhci_rom.c
+++ dump_fch_xhci_rom.c
@@ -12,6 +12,8 @@
 #define ROMTESTCS_TESTMODE_READ	(1 << 1)
 #define ROMTESTCS_DONE	(1 << 30)
 
+#define ROM_SIZE_DWORDS	0x2000
+
 
 void write_60mhz_space_register(struct pci_dev *dev, uint32_t addr, uint32_t value){
 	pci_write_long(dev, INDIRECT_PCI_INDEX_REG, (addr & 0x3FFFFFFF) | 0x40000000);
@@ -25,7 +27,8 @@ uint32_t read_60mhz_space_register(struct pci_dev *dev, uint32_t addr){
 
 
 uint32_t read_rom_dword(struct pci_dev *dev, uint32_t dword_addr){
-	write_60mhz_space_register(dev, IND_60MHZ_SPACE_ROMTESTCS_REG, ROMTESTCS_TEST_START | ROMTESTCS_TESTMODE_READ | ((dword_addr & 0x00001FFF) << 2));
+	uint32_t cmd = ROMTESTCS_TEST_START | ROMTESTCS_TESTMODE_READ | ((dword_addr & (ROM_SIZE_DWORDS-1)) << 2);
+	write_60mhz_space_register(dev, IND_60MHZ_SPACE_ROMTESTCS_REG, cmd);
 	while(!(read_60mhz_space_register(dev, IND_60MHZ_SPACE_ROMTESTCS_REG) & ROMTESTCS_DONE)){;}
 	return read_60mhz_space_register(dev, IND_60MHZ_SPACE_ROMTESTDATA_REG);
 }
@@ -37,7 +40,7 @@ void dump_rom_to_file(struct pci_dev *dev){
 	FILE *fp = fopen(filename, "wb");
 	int i;
 	
-	for(i=0; i<0x2000; i++){
+	for(i=0; i<ROM_SIZE_DWORDS; i++){
 		uint32_t temp = read_rom_dword(dev, i);
 		fwrite(&temp, sizeof(temp), 1, fp);
 	}
@@ -50,16 +53,13 @@ int main(void)
 {
 	struct pci_access *pacc;
 	struct pci_dev *dev;
-	unsigned int c;
-	char namebuf[1024], *name;
 
 	pacc = pci_alloc();
 	pci_init(pacc);
 	pci_scan_bus(pacc);
-	for (dev=pacc->devices; dev; dev=dev->next)
-	{
+	for (dev=pacc->devices; dev; dev=dev->next){
 		pci_fill_info(dev, PCI_FILL_IDENT);
-        //tested with 0x7814, but might still work with 0x7812
+        	//tested with 0x7814, but might still work with 0x7812
 		if(0x1022 == dev->vendor_id && (0x7814 == dev->device_id || 0x7812 == dev->device_id)){
 			dump_rom_to_file(dev);
 		}
